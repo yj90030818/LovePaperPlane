27876e52efbf85288e128247c3ccd27dd34edfd4
Examiner13 <61983631+Examiner13@users.noreply.github.com>
2021-11-04 01:47:04 +0800

    Update Obstacle.java

--- p/Obstacle.java
+++ p/Obstacle.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 
 public class Obstacle extends GameObject{
     boolean live = true;
-    double original_x = 0.0,original_y = 0.0,adjustment = 4.0;
+    double original_x = 0.0,original_y = 0.0,adjustment = 0.0;
 
     public Obstacle(Image img,double x,double y){
         this.img = img;
