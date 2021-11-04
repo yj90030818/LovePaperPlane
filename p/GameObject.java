8ce1a8b6d4c986b0aca0f659deb70925c804e7fa
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-04 16:56:11 +0800

    Update GameObject.java

--- p/GameObject.java
+++ p/GameObject.java
@@ -39,7 +39,7 @@ public class GameObject{
     }
     
     public GameObject(double x, double y, int type, String path, Paperplane plane){
-        GameObject(x, y, type, path);
+        this.GameObject(x, y, type, path);
         this.plane = plane;
     }
     
