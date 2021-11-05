56f746a21aca2fe3eedf540eb4d1dcd06d6557d2
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-05 14:21:16 +0800

    Update Paperplane.java

--- p/Paperplane.java
+++ p/Paperplane.java
@@ -47,7 +47,7 @@ public class Paperplane extends GameObject{
             y = y + 5;
         }
         if(dragFlag){
-            g2.rotate(direction * Angle(Math.abs(x - original_x), Math.abs(original_y - y)), x, y);
+            g2.rotate(direction * Angle(Math.abs(x - original_x), (original_y - y)), x, y);
         }
         g2.drawImage(img,(int)x,(int)y,width * (int)direction,height,null);
     }
