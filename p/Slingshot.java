537825c713b344bd9a49335ac42f4c42bd7f49ec
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-05 15:32:08 +0800

    Update Slingshot.java

--- p/Slingshot.java
+++ p/Slingshot.java
@@ -50,9 +50,13 @@ public class Slingshot extends GameObject{
     
     //滑鼠按下事件
     public void firstpoint(MouseEvent e){
-        shoot = true;
-        pressed_x = e.getX();
-        pressed_y = e.getY();
+        
+        if(e.isMetaDown()){
+            shoot = true;
+            plane.nowDragging();
+            pressed_x = e.getX();
+            pressed_y = e.getY();
+        }
 
     }
 
@@ -64,10 +68,11 @@ public class Slingshot extends GameObject{
         double limit = 70.0;
         double drag_x =(released_x - pressed_x)/4, drag_y=(released_y - pressed_y)/4;
         if(Math.abs(released_x - pressed_x) > 100 || Math.abs(released_y - pressed_y) > 100){
+            if(!(plane.isDragging()))
+                return;
             plane.setX(plane.getoriginal_x() + (Math.abs(drag_x) > limit ? (drag_x > 0 ? limit : -limit) : drag_x));
             plane.setY(plane.getoriginal_y() + (Math.abs(drag_y) > limit ? (drag_y > 0 ? limit : -limit) : drag_y));
             plane.setdirection(pressed_x > released_x ? 1.0 : -1.0);
-            plane.nowDragging();
         }else{
             plane.setX(plane.getoriginal_x());
             plane.setY(plane.getoriginal_y());
