b4e851ef89267a3d2d665e5a59696e829dae2fd2
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-05 15:47:42 +0800

    Update Paperplane.java

--- p/Paperplane.java
+++ p/Paperplane.java
@@ -85,7 +85,7 @@ public class Paperplane extends GameObject{
     }
     
     public void nowDragging(){
-        if(!(go) || live)
+        if(!(go) && live)
             dragFlag = true;
     }
     
