9d294da915f112e4869ffac2d49969a5de9df969
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-05 12:13:18 +0800

    Update Slingshot.java

--- p/Slingshot.java
+++ p/Slingshot.java
@@ -65,7 +65,7 @@ public class Slingshot extends GameObject{
             plane.setAngle(Angle());
         }else{
             plane.setX(plane.getoriginal_x());
-            plane.setY(plane.getoriginal_y())
+            plane.setY(plane.getoriginal_y());
             plane.setAngle(0.0);   
         }
     }
