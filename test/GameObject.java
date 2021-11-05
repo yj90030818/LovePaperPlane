1f4699704ea1bac56254e879e5abfb8403cc20c4
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-05 18:12:28 +0800

    Update GameObject.java

--- test/GameObject.java
+++ test/GameObject.java
@@ -6,15 +6,15 @@ import java.awt.Rectangle;
 //遊戲物件的父類別，讓子類別可以調用父類別的方法
 public class GameObject{
     Image img;
-    int width, height, type;
-    double x, y, v0, angle;
+    int width, height, type, x, y;
+    double v0, angle;
     String ImgResource;
     Paperplane plane = null;
 
 
     public GameObject(double x, double y, int type, String path){
-        this.x = x;
-        this.y = y;
+        this.x = GameUtil.GUI_x(x);
+        this.y = GameUtil.GUI_y(y);
         this.type = type;
         this.ImgResource = path;
         this.img = GameUtil.getImage(type, path);
@@ -28,23 +28,23 @@ public class GameObject{
     }
     
     public void drawSelf(Graphics g){
-        g.drawImage(img, (int)x, (int)y, null);
+        g.drawImage(img, x, y, null);
     }
 
     public void drawSelf(Graphics2D g2){
-        g2.drawImage(img,(int)x,(int)y, null);
+        g2.drawImage(img,x,y, null);
     }
 
     public Rectangle getRect(){
-        return new Rectangle((int)x, (int)y, width, height);
+        return new Rectangle(x, y, width, height);
     }
 
     public void setX(double x){
-        this.x = x;
+        this.x = GameUtil.GUI_x(x);
     }
 
     public void setY(double y){
-        this.y = y;
+        this.y = GameUtil.GUI_y(y);
     }
 
     public void setV0(double v0){
@@ -56,11 +56,11 @@ public class GameObject{
     }
     
     public double getX(){
-        return x;
+        return CO_x(x);
     }
 
     public double getY(){
-        return y;
+        return CO_y(y);
     }
     
     public double getV0(){
