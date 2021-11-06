e671e1c2e13c5e8e164e9de09cc81763d9e788bd
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-06 19:19:57 +0800

    Update GameFrame.java

--- p/GameFrame.java
+++ p/GameFrame.java
@@ -37,7 +37,7 @@ public class GameFrame extends JFrame{
 //     public void launchFrame(){
     public GameFrame(){
         this.setTitle("Love PaperPlane");
-        this.setSize(800 , 500);
+        this.setSize(800 , 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         GamePanel panel = new GamePanel();        
 //         setVisible(true);
