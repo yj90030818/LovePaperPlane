99091c9d75f87d81349a7e5c7097c085d7a1d563
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-25 15:21:52 +0800

    Update GameUtil.java

--- LovePP_0.0.1/GameUtil.java
+++ LovePP_0.0.1/GameUtil.java
@@ -45,12 +45,12 @@ public class GameUtil {
     //Movement--
     public static void nextPosition(GameObject go){
         // System.out.println("(X,Y)="+go.getX()+","+go.getY()+"|(V_x,V_y)="+go.getV_x()+","+go.getV_y());   
-        go.setX(go.getX() + go.getV_x());
-        go.setY(go.getY() + go.getV_y());
+        go.setX(go.getX() + go.getV_x()*0.04);
+        go.setY(go.getY() + go.getV_y()*0.04);
     }
 
     public static void nextV_y(GameObject go){
-        go.setV_y(go.getV_y() - G);
+        go.setV_y(go.getV_y() - G*0.04);
     }
 
     //--
