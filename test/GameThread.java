11c9a0e030ece4eca8a6fd794d5ae665c03da39e
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-27 16:50:16 +0800

    Update GameThread.java

--- test/GameThread.java
+++ test/GameThread.java
@@ -30,7 +30,7 @@ public GameThread(GamePanel panel){
 			time = 0.0;
 		}
                 try{
-                    Thread.sleep(50);
+                    Thread.sleep(1000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }
