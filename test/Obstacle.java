0b9d7cb1b5c941b19b84a96ae3d2346e6f55d139
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-27 14:09:22 +0800

    Update Obstacle.java

--- test/Obstacle.java
+++ test/Obstacle.java
@@ -8,16 +8,6 @@ import java.awt.event.KeyEvent;
 public class Obstacle extends GameObject{
     boolean live = true;
     double original_x = 0.0,original_y = 0.0,adjustment = 0.0,angle = 0.0;
-
-//     public Obstacle(Image img,double x,double y){
-//         this.img = img;
-//         this.x = x;
-//         this.y = y;
-//         this.original_x = x;
-//         this.original_y = y;
-//         this.width = img.getWidth(null);
-//         this.height = img.getHeight(null);
-//     }
     
     public Obstacle(double x, double y, int type, String path){
         super(x, y, type, path);
@@ -25,7 +15,7 @@ public class Obstacle extends GameObject{
         this.original_y = y;
     }
 
-    //繪製紙飛機
+     //計算物理碰撞
     public void drawSelf(Graphics2D g2){
 	if(!live){
 		if(angle < Math.PI / 2.0){
@@ -43,9 +33,3 @@ public class Obstacle extends GameObject{
     	}
 	}
 
-    //計算物理碰撞
-    /*public double collision (int v0,double angle){
-        double radians = Math.toRadians(angle);
-        return 2 * Math.pow(v0,2) * Math.sin(radians) * Math.cos(radians) / 9.8;
-    }*/
-}
