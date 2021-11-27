96553d4f8a3a43512ee0f39d295fdc03660c9e8b
Examiner13 <61983631+Examiner13@users.noreply.github.com>
2021-11-27 23:58:42 +0800

    Update Target.java

--- test/Target.java
+++ test/Target.java
@@ -7,8 +7,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.KeyEvent;
 
 public class Target extends GameObject{
-    boolean live = true,fall = true;
-    double original_x = 0.0,original_y = 0.0,adjustment = 0.0,angle = 0.0;
+    //boolean live = true,fall = true;
+    //double original_x = 0.0,original_y = 0.0,adjustment = 0.0,angle = 0.0;
     
     public Target(double x, double y, int type, String path){
         super(x, y, type, path);
@@ -18,8 +18,6 @@ public class Target extends GameObject{
 
     //繪製紙飛機
     public void drawSelf(Graphics2D g2){
-
-        	g2.drawImage(img,(int)x,(int)y, null);
-	}
-
+	    g2.drawImage(img,(int)x,(int)y, null);
+    }
 }
