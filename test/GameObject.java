edb6c3793dc7144ba0b62dbca4ff85de5c669846
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-11-27 16:01:32 +0800

    Update GameObject.java

--- test/GameObject.java
+++ test/GameObject.java
@@ -22,13 +22,13 @@ public class GameObject{
     }
     
   
-    public void drawSelf(Graphics g){
-        g.drawImage(img, (int)x, (int)y, null);
-    }
+//     public void drawSelf(Graphics g){
+//         g.drawImage(img, (int)x, (int)y, null);
+//     }
 
-    public void drawSelf(Graphics2D g2){
-        g2.drawImage(img,(int)x,(int)y, null);
-    }
+//     public void drawSelf(Graphics2D g2){
+//         g2.drawImage(img,(int)x,(int)y, null);
+//     }
 
     public Rectangle getRect(){
         return new Rectangle((int)x, (int)y, width, height);
