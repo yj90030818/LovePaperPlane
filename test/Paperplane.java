bcaaeb90b5aef3dc3ac32a4197f65b1895f682eb
yj90030818 <61908701+yj90030818@users.noreply.github.com>
2021-12-01 16:03:40 +0800

    Update Paperplane.java

--- test/Paperplane.java
+++ test/Paperplane.java
@@ -11,6 +11,8 @@ public class Paperplane extends GameObject{
     double original_x = 0.0,original_y = 0.0,change = 0.0,adjustment = 4.0, direction = 1.0,x0 = 0.0,y0 = 0.0;
     double angle = 0.0;	
     
+	public Paperplane(){}
+	
     public Paperplane(double x, double y, int type, String ImgResource){
         super(x, y, type, ImgResource);
         this.original_x = x;
